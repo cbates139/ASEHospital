e49dfe0a0f5aef7bab4f49f431e68ffc607af5bd
Callum <calbat@live.co.uk>
2022-04-05 16:16:27 +0100

    Updated DB

--- HospitalDB/dbo/Tables/Pii.sql
+++ HospitalDB/dbo/Tables/Pii.sql
@@ -1,6 +1,6 @@
 ﻿-- Secondary Tables
 CREATE TABLE [dbo].[Pii] (
-    PatientID VARCHAR(20)  NOT NULL PRIMARY KEY,
+    PatientID INT  NOT NULL PRIMARY KEY,
     FirstName VARCHAR (20)  NOT NULL,
     LastName VARCHAR (20) NOT NULL,
     Dob DATETIME NOT NULL,
