eb8bd6fbadf67e9733a3045399ab8d1b71659637
Callum Batess <calbat@live.co.uk>
2022-04-13 14:09:39 +0100

    Add files via upload
    Updated CreateStatements to reflect final version of ERD

--- CreateStatements.sql
+++ CreateStatements.sql
@@ -112,4 +112,10 @@ CREATE TABLE [dbo].[Service] (
     CONSTRAINT FK_Service_LID FOREIGN KEY (LeaderID) REFERENCES [dbo].[Staff](StaffID),
     CONSTRAINT FK_Service_MID FOREIGN KEY (MemberID) REFERENCES [dbo].[Staff](StaffID)
 );
+CREATE TABLE [dbo].[RecordStaff] (
+    StaffID INT NOT NULL,
+    RecordID INT NOT NULL,
+    CONSTRAINT FK_RS_SID FOREIGN KEY (StaffID) REFERENCES [dbo].[Staff](StaffID),
+    CONSTRAINT FK_RS_RID FOREIGN KEY (RecordID) REFERENCES [dbo].[Record](RecordID)
+);
 
