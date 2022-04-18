15f9824c7c256ee6d60ed1174da68672eee90053
Callum Batess <calbat@live.co.uk>
2022-04-18 15:51:50 +0100

    Revert "Merge"

--- Hospital/User Controls/Admin/AdminPatientManagement.Designer.cs
+++ Hospital/User Controls/Admin/AdminPatientManagement.Designer.cs
@@ -29,87 +29,32 @@ namespace Hospital.User_Controls.Admin
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRegisterPatient = new System.Windows.Forms.Button();
-            this.btnUpdatePatientInfo = new System.Windows.Forms.Button();
-            this.btnDeletePatient = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.imageLogo = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.imageLogo)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnRegisterPatient
+            // label1
             // 
-            this.btnRegisterPatient.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnRegisterPatient.Location = new System.Drawing.Point(336, 234);
-            this.btnRegisterPatient.Name = "btnRegisterPatient";
-            this.btnRegisterPatient.Size = new System.Drawing.Size(263, 45);
-            this.btnRegisterPatient.TabIndex = 1;
-            this.btnRegisterPatient.Text = "Register Pation";
-            this.btnRegisterPatient.UseVisualStyleBackColor = true;
-            this.btnRegisterPatient.Click += new System.EventHandler(this.btnRegisterPatient_Click);
-            // 
-            // btnUpdatePatientInfo
-            // 
-            this.btnUpdatePatientInfo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnUpdatePatientInfo.Location = new System.Drawing.Point(336, 326);
-            this.btnUpdatePatientInfo.Name = "btnUpdatePatientInfo";
-            this.btnUpdatePatientInfo.Size = new System.Drawing.Size(263, 45);
-            this.btnUpdatePatientInfo.TabIndex = 2;
-            this.btnUpdatePatientInfo.Text = "Update Patient Info";
-            this.btnUpdatePatientInfo.UseVisualStyleBackColor = true;
-            // 
-            // btnDeletePatient
-            // 
-            this.btnDeletePatient.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnDeletePatient.Location = new System.Drawing.Point(336, 419);
-            this.btnDeletePatient.Name = "btnDeletePatient";
-            this.btnDeletePatient.Size = new System.Drawing.Size(263, 45);
-            this.btnDeletePatient.TabIndex = 3;
-            this.btnDeletePatient.Text = "Delete Patient";
-            this.btnDeletePatient.UseVisualStyleBackColor = true;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(320, 129);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(315, 45);
-            this.label8.TabIndex = 51;
-            this.label8.Text = "Patient Managment";
-            // 
-            // imageLogo
-            // 
-            this.imageLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.imageLogo.Location = new System.Drawing.Point(3, 3);
-            this.imageLogo.Name = "imageLogo";
-            this.imageLogo.Size = new System.Drawing.Size(100, 100);
-            this.imageLogo.TabIndex = 52;
-            this.imageLogo.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin Patient Management";
             // 
             // AdminPatientManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.imageLogo);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.btnDeletePatient);
-            this.Controls.Add(this.btnUpdatePatientInfo);
-            this.Controls.Add(this.btnRegisterPatient);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.label1);
             this.Name = "AdminPatientManagement";
-            this.Size = new System.Drawing.Size(998, 566);
-            ((System.ComponentModel.ISupportInitialize)(this.imageLogo)).EndInit();
+            this.Size = new System.Drawing.Size(926, 615);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button btnRegisterPatient;
-        private System.Windows.Forms.Button btnUpdatePatientInfo;
-        private System.Windows.Forms.Button btnDeletePatient;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.PictureBox imageLogo;
+
+        private System.Windows.Forms.Label label1;
     }
 }
--- Hospital/User Controls/Junior/JuniorPatientView.cs
+++ Hospital/User Controls/Junior/JuniorPatientView.cs
@@ -5,18 +5,17 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-using Hospital.Models;
 
 namespace Hospital.User_Controls
 {
     public partial class JuniorPatientView : UserControl
     {
-        public RecordModel addR { get; private set; }
         public JuniorPatientView()
         {
             InitializeComponent();
             InitialiseUserControl();
         }
+
         private void InitialiseUserControl()
         {
             patientDetail_panel.Visible = false;
@@ -30,27 +29,31 @@ namespace Hospital.User_Controls
             treatmentList_listbox.Visible = false;
 
             medication_label.Visible = false;
-            medicationList_listbox.Visible = false;
+            dataGridView1.Visible = false;
 
             reasonForVisit_label.Visible = false;
             notesText_textbox.Visible = false;
 
             treatmentSubmit_button.Visible = false;     
         }
+
         private void InitialisePatientList()
         {
             //TODO: update patient list from database
         }
         private void InitialiseMedication()
         {
-            medicationList_listbox.Items.Clear();
-            medicationList_listbox.Items.Add("Morphine");
-            medicationList_listbox.Items.Add("Oxycodone");
-            medicationList_listbox.Items.Add("Buscopan");
-            medicationList_listbox.Items.Add("Amitriptyline");
-            medicationList_listbox.Items.Add("Tramadol");
-            medicationList_listbox.Items.Add("Codeine");
-            medicationList_listbox.Items.Add("Paracetamol");
+            //TODO: update medication from database
+        }
+
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void panel13_Paint(object sender, PaintEventArgs e)
+        {
+
         }
 
         private void viewPatientList_button_Click(object sender, EventArgs e)
@@ -76,45 +79,32 @@ namespace Hospital.User_Controls
                 treatmentList_listbox.Visible = true;
 
                 medication_label.Visible = true;
-                medicationList_listbox.Visible = true;
+                dataGridView1.Visible = true;
 
                 reasonForVisit_label.Visible = true;
                 notesText_textbox.Visible = true;
 
                 treatmentSubmit_button.Visible = true;
+
             }
         }
+        
         private void treatmentList_listbox_SelectedIndexChanged(object sender, EventArgs e)
-        {   //Display Medication if Selected
-            if (treatmentList_listbox.SelectedIndex == 0)
+        {
+            // Get the currently selected item in the ListBox.
+            string curItem = treatmentList_listbox.SelectedItem.ToString();
+            //Display Medication if Selected
+            if (curItem == "Medication")
                 InitialiseMedication();
-            else
-                medicationList_listbox.Items.Clear();
+
         }
         private void treatmentSubmit_button_Click(object sender, EventArgs e)
         {
-            int StaffID = 1;
-            int PatientID = 1;
-            int RoomID = 1;
-            string ReasonForVisit = notesText_textbox.Text;
-
-            if (notesText_textbox.Text == "")
+            /*
+            if(validation)
             {
-                MessageBox.Show("Please Provide Reason For Visit");
-                return;
-            }else if(treatmentList_listbox.SelectedIndex == -1)
-            {
-                MessageBox.Show("Please Provide Treatment Type For Visit");
-                return;
-            }
-            else if((treatmentList_listbox.SelectedIndex == 0) && (medicationList_listbox.SelectedIndex == -1))
-            {
-                 MessageBox.Show("Please Provide Medication For Visit");
-            }
-            else 
-            {             
-                RecordModel record = new Models.RecordModel(StaffID, PatientID, RoomID, ReasonForVisit);
-                {   //Close Patient Treat Patient Details
+                //TODO: Submit data to database - validate
+                {
                     patientDetail_panel.Visible = false;
                     submitTreatment_panel.Visible = false;
 
@@ -125,16 +115,21 @@ namespace Hospital.User_Controls
                     treatmentList_listbox.Visible = false;
 
                     medication_label.Visible = false;
-                    medicationList_listbox.Visible = false;
+                    dataGridView1.Visible = false;
 
-                    reasonForVisit_label.Visible = false;
+                    notes_label.Visible = false;
                     notesText_textbox.Visible = false;
 
                     treatmentSubmit_button.Visible = false;
-                    patientList_listbox.SelectedIndex = -1;
                 }
                 MessageBox.Show("Record Submitted");
-            }             
+            }
+            else
+            {
+                MessageBox.Show("Error With Data");
+            }
+             */
         }
+
     }
 }
